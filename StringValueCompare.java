fc8568c037302d91904a023ec1c0cabd78ae641d
ChinnChuuKen <zhongqianchen0@gmail.com>
2017-05-17 12:52:45 +0900

    diyicixiugai

---
+++ StringValueCompare.java
@@ -0,0 +1,15 @@
+class StringValueCompare{
+	public static void main(String[] args){
+		
+		
+		
+		//変数strの値がMSRか否か判定する記述
+		//を書いてください。
+		
+		
+			System.out.println("変数strの値がMSR。");
+		}else {
+			System.out.println("変数strの値がMSRではありません。");
+		}
+	}
+}
